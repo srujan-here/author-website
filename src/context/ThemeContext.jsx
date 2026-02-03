b24de4af57877ea89b13c59de8fabfc1f414dc72
Srujan Putta <srujanputta@Srujans-MacBook-Air.local>
2026-02-03 16:37:19 +0530

    fixes

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -3,7 +3,7 @@ import { createContext, useContext, useEffect, useState } from 'react';
 const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
-    const [theme, setTheme] = useState(localStorage.getItem('theme') || 'light');
+    const [theme, setTheme] = useState(localStorage.getItem('theme') || 'dark');
 
     useEffect(() => {
         const root = window.document.documentElement;
